f80e9d6f1984d82722da3005474b2f0c4783bb77
Thomas Hartmann <thomas.hartmann@th-ht.de>
2018-08-29 15:39:42 +0200

    add FT testing data for most major systems and decrease size (#39)
    * deleted old fieldtrip testing data
    
    * new version of fieldtrip testdata creating scripts for most major systems
    
    * added new fieldtrip testing data
    
    * incremented version

--- fieldtrip/create_fieldtrip_data.m
+++ fieldtrip/create_fieldtrip_data.m
@@ -2,12 +2,6 @@
 clear all global
 restoredefaultpath
 
-%% set variable
-path_to_fiff = '../MEG/sample';
-raw_fiff_fname = 'sample_audvis_trunc_raw.fif';
-
-results_path = 'from_mne_sample';
-
 %% get fieldtrip folder from ENV variable
 ft_path = getenv('FT_PATH');
 
@@ -15,40 +9,73 @@ ft_path = getenv('FT_PATH');
 addpath(ft_path)
 ft_defaults
 
-%% load the whole raw fiff file
+%% create neuromag dataset....
 cfg = [];
-cfg.trialdef.triallength = Inf;
-cfg.dataset = fullfile(path_to_fiff, raw_fiff_fname);
+cfg.name = 'neuromag306';
+cfg.file_name = 'MEG/sample/sample_audvis_trunc_raw.fif';
+cfg.eventtype = 'STI 014';
+cfg.eventvalue = [1, 2, 3, 4, 5];
+cfg.crop = 5;
+cfg.remove_channels = [10, 20, 30];
 
-cfg = ft_definetrial(cfg);
+create_ft_data_fun(cfg);
 
-data = ft_preprocessing(cfg);
-
-%% save the raw data...
-save(fullfile(results_path, 'raw_v7.mat'), '-v7', 'data')
-save(fullfile(results_path, 'raw_v73.mat'), '-v7.3', 'data')
-
-%% get epoched data
+%% create BTI dataset...
 cfg = [];
-cfg.trialdef.eventtype = 'STI 014';
-cfg.trialdef.eventvalue = [1, 2, 3];
-cfg.trialdef.prestim = 0.05;
-cfg.trialdef.poststim = 0.05;
-cfg.dataset = fullfile(path_to_fiff, raw_fiff_fname);
+cfg.name = 'BTI';
+cfg.file_name = 'BTi/erm_HFH/c,rfDC';
+cfg.eventtype = 'None';
+cfg.eventvalue = [1, 2];
+cfg.remove_channels = [10, 20, 30];
 
-cfg = ft_definetrial(cfg);
+create_ft_data_fun(cfg);
 
-data = ft_preprocessing(cfg);
-
-%% save the epoched data...
-save(fullfile(results_path, 'epoched_v7.mat'), '-v7', 'data')
-save(fullfile(results_path, 'epoched_v73.mat'), '-v7.3', 'data')
-
-%% run timelockanalysis
+%% create CNT dataset...
 cfg = [];
+cfg.name = 'CNT';
+cfg.file_name = 'CNT/scan41_short.cnt';
+cfg.eventtype = 'trigger';
+cfg.eventvalue = [0, 7, 109];
+cfg.crop = 5;
+cfg.remove_channels = [10, 20, 30];
 
-data = ft_timelockanalysis(cfg, data);
+create_ft_data_fun(cfg);
 
-%% save the averaged data...
-save(fullfile(results_path, 'averaged_v7.mat'), '-v7', 'data')
-save(fullfile(results_path, 'averaged_v73.mat'), '-v7.3', 'data')
+%% create CTF dataset...
+cfg = [];
+cfg.name = 'CTF';
+cfg.file_name = 'CTF/testdata_ctf_mc.ds';
+cfg.eventtype = 'HTRG001';
+cfg.eventvalue = [1];
+cfg.crop = 0.15;
+cfg.remove_channels = [10, 20, 30];
+
+create_ft_data_fun(cfg);
+
+%% create EGI dataset...
+cfg = [];
+cfg.name = 'EGI';
+cfg.file_name = 'EGI/test_egi.mff';
+cfg.eventtype = {'DIN1', 'DIN2', 'DIN3'};
+cfg.eventvalue = {'DIN1', 'DIN2', 'DIN3'};
+cfg.crop = 8;
+cfg.remove_channels = [10, 20, 30];
+
+create_ft_data_fun(cfg);
+
+%% create eximia dataset
+cfg = [];
+cfg.name = 'eximia';
+cfg.file_name = 'eximia/test_eximia.nxe';
+cfg.crop = 0.15;
+cfg.remove_channels = [10, 20, 30];
+
+create_ft_data_fun(cfg);
+
+%% create KIT dataset...
+cfg = [];
+cfg.name = 'KIT';
+cfg.file_name = 'KIT/test_as-raw.con';
+cfg.remove_channels = [10, 20, 30];
+
+create_ft_data_fun(cfg);
